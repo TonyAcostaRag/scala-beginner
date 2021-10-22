924477a08cb23626bb7322eeda90b71f8e632c7b
Antonio-Acosta <antonio.acosta@ibm.com>
2021-10-22 11:08:29 -0500

    Declaration of String valiables type specified and inferred

--- src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -3,4 +3,10 @@ package lectures.part1basics
 object ValuesVariablesTypes extends App {
   val x: Int = 42
   println(x)
+  
+  val aString: String = "String var"
+  val bString = "Inferred String"
+  
+  println(aString)
+  println(bString)
 }
