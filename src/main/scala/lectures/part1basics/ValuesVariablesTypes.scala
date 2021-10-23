c5379127c672872a2a625994e3bb583e0224a97b
Antonio-Acosta <antonio.acosta@ibm.com>
2021-10-22 22:04:27 -0500

    Adding boolean type and character type

--- src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -2,11 +2,23 @@ package lectures.part1basics
 
 object ValuesVariablesTypes extends App {
   val x: Int = 42
-  println(x)
+  val y = 30
+  println("Integer in scala: " + x)
+  println("Inferred integer in scala: " + y)
   
   val aString: String = "String var"
   val bString = "Inferred String"
+  println("String in scala: " + aString)
+  println("Inferred string in scala: " + bString)
   
-  println(aString)
-  println(bString)
+  val abolean: Boolean = true
+  val bbolean = false
+  println("Boolean in scala: " + abolean)
+  println("Inferred boolean in scala: " + bbolean)
+  
+  val achar: Char = 'a'
+  val bchar = 'b'
+  println("Character variable: " + achar)
+  println("Inferred character variable: " + bchar)
+  
 }
