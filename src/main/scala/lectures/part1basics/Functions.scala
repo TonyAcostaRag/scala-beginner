bb33e40bc73ec0667e197dac719d06382f094f09
Antonio-Acosta <antonio.acosta@ibm.com>
2021-10-31 20:15:27 -0600

    adding functions class

--- src/main/scala/lectures/part1basics/Functions.scala
+++ src/main/scala/lectures/part1basics/Functions.scala
@@ -1,5 +1,23 @@
 package lectures.part1basics
 
-object Functions {
+object Functions extends App {
+  
+  def aFunction (a: String, b: Int): String = {
+    a + " " + b
+  }
+  
+  println(aFunction("Concatenate function", 3))
+  
+  def aParameterlessFunction(): Int = 42
+  println(aParameterlessFunction())
+  println(aParameterlessFunction)
+  
+  def aRepeatedFunction(aString: String, n: Int): String = {
+    if (n == 1) aString
+    else aString + aRepeatedFunction(aString, n-1)
 
+  }
+  
+  println(aRepeatedFunction("Repeated string", 10))
+  
 }
