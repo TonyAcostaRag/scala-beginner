9536dbbc9d1f8aef0ffacc1a67da91009eac3fad
Antonio-Acosta <antonio.acosta@ibm.com>
2021-11-01 00:29:58 -0600

    Updating Functions class - print name, factorial and fibonacci

--- src/main/scala/lectures/part1basics/Functions.scala
+++ src/main/scala/lectures/part1basics/Functions.scala
@@ -32,4 +32,27 @@ object Functions extends App {
 
   println(aBigFunction(3))
   
+  def aFunctionWhichPrintsNameAndAge(name: String, age: Int): String = 
+   "Hello my name is " + name + " and I'm " + age + " years old."
+
+  println(aFunctionWhichPrintsNameAndAge("TonyAcostaRag", 20))
+  
+  def aFactorialFunction(n: Int): Int = {
+    if (n > 1) n * aFactorialFunction(n - 1)
+    else 1
+  }
+
+  println(aFactorialFunction(1))
+  println(aFactorialFunction(2))
+  println(aFactorialFunction(3))
+  println(aFactorialFunction(4))
+  println(aFactorialFunction(5))
+ 
+  def fibonacciNumberPosition(n: Int): Int = {
+    if (n <= 2) 1
+    else fibonacciNumberPosition(n - 1) + fibonacciNumberPosition(n - 2)
+  }
+  
+  println(fibonacciNumberPosition(8))
+  
 }
