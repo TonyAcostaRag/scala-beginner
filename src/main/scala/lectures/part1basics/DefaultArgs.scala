8ea259cafb28d29094cb317f2eeb7d0c1dc425da
Antonio-Acosta <antonio.acosta@ibm.com>
2021-11-23 21:59:47 -0600

    Default and Named Args functions

---
+++ src/main/scala/lectures/part1basics/DefaultArgs.scala
@@ -0,0 +1,20 @@
+package lectures.part1basics
+
+object DefaultArgs extends App {
+
+  def trFact(n: Int, acc: Int): Int = {
+    if (n <= 1) acc
+    else trFact(n-1, n*acc)
+  }
+  
+  val fact10 = trFact(10, 1)
+  
+  def savePicture(format: String = "jpg", width: Int = 1920, height: Int = 1080): Unit =
+    println("saving picture")
+    
+  savePicture(format = "bmp")
+  savePicture("bmp", height = 900)
+  
+  
+  
+}
